47f930356c31b1025edcad96207623af36f5e79b
Dominik Bleidorn <dominikr.bleidorn@tu-dortmund.de>
2022-06-16 22:48:36 +0200

    Version Push

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="GanttPlotter",
     packages=["GanttPlotter"],
-    version="0.0.1",
+    version="0.0.3",
     install_requires=[
         "matplotlib==3.5.1",
     ],
