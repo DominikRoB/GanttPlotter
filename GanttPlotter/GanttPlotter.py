5db6a2c4afaf8107836f5212932e352883c1d3d3
Cristina Pena <mariacristina.penafuentes@inosim.com>
2023-05-24 15:25:46 +0200

    Add labeling to jobs and separate changeover activities from jobs.

--- GanttPlotter/GanttPlotter.py
+++ GanttPlotter/GanttPlotter.py
@@ -5,13 +5,13 @@
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import hsv_to_rgb
-import math
+import math, os, __main__
 from collections import defaultdict
 from datetime import datetime, date
 
 
 class GanttPlotter:
-    def __init__(self, resources=None, jobs=None):
+    def __init__(self, resources=None, jobs=None,experiment_date=0,episode = 0):
         if resources is None:
             resources = []
         if jobs is None:
@@ -20,7 +20,8 @@ class GanttPlotter:
         self._barheight = 9
         self._jobs = jobs
         self._resources = resources
-
+        self._experiment_date = experiment_date
+        self._episode = episode
         self._job_color_dict = {}
 
     def show_gantt(self):
@@ -40,7 +41,7 @@ class GanttPlotter:
             yticklabels.append(resource_name)
             ytick_value = origin_offset + (count + 1) * self._tickdistance
             yticks.append(ytick_value)
-
+            yticklabels.sort(reverse=True)
         return yticks, yticklabels
 
     def _get_bar_height(self, resource):
@@ -87,11 +88,12 @@ class GanttPlotter:
             hue = math.fmod(i * 1/golden_ratio, 1.0)
             saturation = 0.5
             value = math.sqrt(1.0 - math.fmod(i * 1/golden_ratio, 0.5))
+
             next_color = hsv_to_rgb([hue, saturation, value])
             colors.append(next_color)
         return colors
 
-    def generate_gantt(self, title="",description="", save_to_disk=False):
+    def generate_gantt(self, title="Kopanos Scheduling Problem",description="", save_to_disk=True):
 
         fig, gnt = plt.subplots()
         yticks, yticklabels = self._find_yticks()
@@ -104,7 +106,7 @@ class GanttPlotter:
         x_maxlim = self._find_xmaxlim()
         gnt.set_xlim(0, x_maxlim)
 
-        gnt.set_xlabel("seconds since start")
+        gnt.set_xlabel("Episode duration [h]")
         gnt.set_ylabel("Resources")
 
         gnt.set_yticks(yticks)
@@ -131,9 +133,21 @@ class GanttPlotter:
                 (lower_yaxis, self._barheight),
                 facecolors=facecolors,
                 alpha=0.9,
+                linewidth = 0.7,
                 edgecolor="black",
             )
             resource_count = resource_count + 1
+
+            # Add labels to the rectangles
+            labels = [job.name for job in job_list]
+            values = [job.duration for job in job_list]
+            for i, bar in enumerate(broken_bars):
+                x = (bar[0] + (bar[1]/2) ) # calculate the x position of the label
+                y = lower_yaxis + self._barheight / 2  # calculate the y position of the label
+                if labels[i] != "CHANGEOVER":
+                    gnt.text(x, y, labels[i], rotation = 45, ha='center', va="center",fontsize=9,fontweight="bold",color='black')  # set the font size of the label)
+                #else:
+                #    gnt.text(x, y, values[i], rotation = 45, ha = "center", va="center", fontsize=9, fontweight="bold", color='black')
 
         self._add_footnote(gnt)
 
@@ -152,10 +166,9 @@ class GanttPlotter:
                                 label=element_label)
             legend_elements.append(new_element)
 
-        max_elements_per_column = 20
+        max_elements_per_column = 11
         legend_columns = math.floor(len(legend_elements) / max_elements_per_column)+1
-        # plt.legend(["Test1", "Test2"], legend_labels, bbox_to_anchor=(1.04, 0.6), loc="upper left")
-        plt.legend(handles=legend_elements, bbox_to_anchor=(1.04, 0.5), loc="center left", ncol=legend_columns)
+        #plt.legend(handles=legend_elements, bbox_to_anchor=(1, 1),loc="upper right", ncol=legend_columns, fontsize=9)
 
         if description:
             props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
@@ -167,13 +180,17 @@ class GanttPlotter:
                      ha="center"
                      )
 
-
-
         fig.tight_layout()
         if save_to_disk:
             now = datetime.now()
-            dt_string = now.strftime("%d-%m-%Y--%H-%M-%S")
-            plt.savefig(f"{dt_string}--Gantt-{title.replace(' ', '_')}.png")
+            script = os.path.basename(__main__.__file__)
+            filename = f"Results/{script}/{self._experiment_date}/Evaluation/Episode {self._episode}/{self._experiment_date}_{self._episode}_{title.replace(' ', '_')}.png"
+            os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+            figure = plt.gcf()  # get current figure
+            figure.set_size_inches(16,9)  # set figure's size manually to your full screen (32x18)
+            plt.savefig(filename,dpi=400)  # bbox_inches removes extra white spaces
+
         return fig
 
     def _generate_bars_for_resource(self, resource, job_list):
@@ -199,7 +216,7 @@ class GanttPlotter:
         # Declaring a figure "gnt"
         fig, gnt = plt.subplots()
 
-        gnt.set_title("Example")
+        gnt.set_title("Kopanos Scheduling Problem")
 
         # Setting Y-axis limits
         gnt.set_ylim(0, 50)
@@ -208,7 +225,7 @@ class GanttPlotter:
         gnt.set_xlim(0, 160)
 
         # Setting labels for x-axis and y-axis
-        gnt.set_xlabel("seconds since start")
+        gnt.set_xlabel("Episode duration [h]")
         gnt.set_ylabel("Processor")
 
         # Setting ticks on y-axis
@@ -245,6 +262,8 @@ class GanttPlotter:
         unique_job_names = set([job.name for job in self._jobs])
         for count, job in enumerate(unique_job_names):
             self._job_color_dict[job] = colors[count]
+            if job == "CHANGEOVER":
+                self._job_color_dict[job] = hsv_to_rgb([180,0.1,1])
 
 
 class GanttJob:
@@ -253,31 +272,3 @@ class GanttJob:
         self.duration = duration
         self.resource = resource
         self.name = name
-
-
-if __name__ == "__main__":
-    resources = ["Unit 1", "Unit 2", "Unit 3"]
-    start_time = 40
-    duration = 50
-    resource = "Unit 1"
-    job_name = "Job1"
-    task1 = GanttJob(start_time, duration, resource, job_name)
-
-    task2 = GanttJob(110, 10, "Unit 2", "Job2")
-    task3 = GanttJob(150, 10, "Unit 2", "Job1")
-
-    task4 = GanttJob(10, 50, "Unit 3", "Job3")
-    task5 = GanttJob(110, 30, "Unit 3", "Job2")
-    task6 = GanttJob(130, 20, "Unit 3", "Job3")
-    task_list = [task1, task2, task3, task4, task5, task6]
-
-    my_plotter = GanttPlotter(resources=resources, jobs=task_list)
-
-    new_resource = "Unit 4"
-    new_task = GanttJob(70, 15, "Unit 4", "Job4")
-
-    my_plotter.add_resource(new_resource)
-    my_plotter.add_job(new_task)
-
-    my_plotter.generate_gantt("Great Gantt Generation")
-    my_plotter.show_gantt()
