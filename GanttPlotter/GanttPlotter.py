5ebe20f6a77f9d3e1774645e5b9ca1db90e5cc0d
Dominik Bleidorn <dominikr.bleidorn@tu-dortmund.de>
2023-05-25 14:02:36 +0200

    Remove unneccesary example method

--- GanttPlotter/GanttPlotter.py
+++ GanttPlotter/GanttPlotter.py
@@ -560,47 +560,6 @@ class GanttPlotter:
             fontsize=8,
         )
 
-    def example_gantt(self):
-        # Declaring a figure "gnt"
-        fig, gnt = plt.subplots()
-
-        gnt.set_title("Kopanos Scheduling Problem")
-
-        # Setting Y-axis limits
-        gnt.set_ylim(0, 50)
-
-        # Setting X-axis limits
-        gnt.set_xlim(0, 160)
-
-        # Setting labels for x-axis and y-axis
-        gnt.set_xlabel("Episode duration [h]")
-        gnt.set_ylabel("Processor")
-
-        # Setting ticks on y-axis
-        gnt.set_yticks([15, 25, 35])
-        # Labelling tickes of y-axis
-        gnt.set_yticklabels(["1", "2", "3"])
-
-        # Setting graph attribute
-        gnt.grid(True)
-
-        # Declaring a bar in schedule
-        gnt.broken_barh([(40, 50)], (30, 9), facecolors=("tab:orange"))
-
-        # Declaring multiple bars in at same level and same width
-        gnt.broken_barh(
-            [(110, 10), (150, 10)], (10, 9), facecolors=["tab:blue", "tab:orange"]
-        )
-
-        gnt.broken_barh(
-            [(10, 50), (100, 20), (130, 10)], (20, 9), facecolors=("tab:red")
-        )
-
-        self._add_footnote(gnt)
-
-        fig.tight_layout()
-        return fig
-
     def _get_color_for(self, job_name):
         return self._job_color_dict[job_name]
 
